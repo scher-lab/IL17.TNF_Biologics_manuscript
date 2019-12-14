58f15d5745987630bb52a6c67c46cdd75d8da2ad
Scher Lab <57496836+scher-lab@users.noreply.github.com>
2019-12-14 11:37:38 -0500

    Update 07_Figure.05_Supplementary.figure.06.R

--- 07_Figure.05_Supplementary.figure.06.R
+++ 07_Figure.05_Supplementary.figure.06.R
@@ -1,18 +1,21 @@
-############################################ 
-## R script                               ##
-## Project: IL17.TNF_Biologics_manuscript ##
-## Figure 05, Supplementary Figure 06	  ##
-## ITS data				  ##
-## Author: JM                             ##
-############################################
+################################################################ 
+## R script                                                   ##
+## Project: IL17.TNF Biologics manuscript                     ##
+## Figure 05, Supplementary figure 06, Supplementary table 04 ##
+## ITS data				                      ##
+## Author: JM                                                 ##
+################################################################
 
 ### Brief description:
-### This script covers the code for Figure 05 and Supplementary Figure 06.
+### This script covers the code for Figure 05, Supplementary figure 06 and Supplemenatry table 04
 
-### Figure 05, Supplementary Figure 06:
+### Figure 05, Supplementary figure 06:
 ### Panels A and D: TNFi relative abundance lineplots
 ### Panels B and E: IL-17i relative abundance lineplots
 ### Panels C and F: Boxplots representing magnitude of relative abundance change in TNFi and IL-17i subsets
+
+### Supplementary table 04:
+### P-values
 
 ############################################################################
 ############################################################################
@@ -545,3 +548,301 @@ for (i in seq_along(pseqs)) {
     capture.output(mw.maint, file = fs, append = TRUE)
   }
 }
+
+###########################################################################
+############################################################################
+############################################################################
+
+### Table of p-values of taxa relative abundance between pre and post visits ###
+
+# list of phyloseq objects to process
+pseqs <- list(physeq = phy_ITS.R1_human_TNF.B.C_IL17.B.C.D)
+
+# list of specific taxa
+taxa.names <- c("p__Ascomycota",
+                "c__Saccharomycetes", "c__Tremellomycetes",
+                "o__Saccharomycetales", "o__Tremellales", 
+                "f__Saccharomycetaceae", "f__Mycosphaerellaceae", "f__Glomerellaceae",
+                "f__Plectosphaerellaceae", "f__Sclerotiniaceae", "f__Mucoraceae",
+                "f__Incertae_sedis",
+                "g__Saccharomyces", "g__Candida", "g__Penicillium", 
+                "g__Debaryomyces", "g__Glomerella", "g__Plectosphaerella",
+                "g__Aspergillus",
+                "s__Pichia_sp_1_TMS_2011", "s__Penicillium_charlesii", "s__Hypocrea_pseudokoningii",
+                "s__Fusarium_sp_4_TMS_2011", "s__Candida_fermenticarens", "s__Trichoderma_longibrachiatum",
+                "s__Rhodotorula_sp_RhSoW01", "s__Aspergillus_niger", "s__Saccharomyces_cerevisiae", 
+                "s__Candida_albicans", "s__Candida_dubliniensis", "s__Candida_parapsilosis", 
+                "s__Candida_tropicalis") 
+
+# for each phyloseq object in pseqs, calculate the relative abudnance
+# over time for specific taxa
+for (i in seq_along(pseqs)) {
+  
+  # merge phyloseq objects to each taxnomic level
+  phylum <- tax_glom(pseqs[[i]], taxrank = "Phylum", NArm = FALSE)
+  class <- tax_glom(pseqs[[i]], taxrank = "Class", NArm = FALSE)
+  order <- tax_glom(pseqs[[i]], taxrank = "Order", NArm = FALSE)
+  family <- tax_glom(pseqs[[i]], taxrank = "Family", NArm = FALSE)
+  genus <- tax_glom(pseqs[[i]], taxrank = "Genus", NArm = FALSE)
+  species <- tax_glom(pseqs[[i]], taxrank = "Species", NArm = FALSE)
+  
+  # transform to relative abundance
+  tp <- transform_sample_counts(phylum, rel_abundance)
+  tc <- transform_sample_counts(class, rel_abundance)
+  to <- transform_sample_counts(order, rel_abundance)
+  tf <- transform_sample_counts(family, rel_abundance)
+  tg <- transform_sample_counts(genus, rel_abundance)
+  ts <- transform_sample_counts(species, rel_abundance)
+  
+  # create table for storing data
+  p.val <- matrix(data = NA, nrow = 32, ncol = 4)
+  colnames(p.val) <- c("Taxa", "p.value.TNF", "p.value.IL17.load", "p.value.IL17.maint")
+  
+  for (j in seq_along(taxa.names)) {
+    
+    # for each taxon, create new directory to store results
+    dir.taxa = paste(".../IL17.TNF/ITS/jobs/5_specific.taxa_rel.abund.wilcox_R/", taxa.names[j], "/", sep = "")
+    if (dir.exists(dir.taxa) == FALSE) {
+      dir.create(dir.taxa)
+    }
+    
+    # extract relative abundance of specific taxa
+    if (grepl("p__", taxa.names[j])) {
+      taxa.rel.abund <- subset_taxa(tp, Phylum == taxa.names[j])
+    } else if (grepl("c__", taxa.names[j])) {
+      taxa.rel.abund <- subset_taxa(tc, Class == taxa.names[j])
+    } else if (grepl("o__", taxa.names[j])) {
+      taxa.rel.abund <- subset_taxa(to, Order == taxa.names[j])
+    } else if (grepl("f__", taxa.names[j])) {
+      taxa.rel.abund <- subset_taxa(tf, Family == taxa.names[j])
+    } else if (grepl("g__", taxa.names[j])) {
+      taxa.rel.abund <- subset_taxa(tg, Genus == taxa.names[j])
+    } else if (grepl("s__", taxa.names[j])) {
+      taxa.rel.abund <- subset_taxa(ts, Species == taxa.names[j])
+    }
+    
+    # create dataset
+    d <- as.data.frame(merge(sample_data(taxa.rel.abund), t(otu_table(taxa.rel.abund)), 
+                             by = "row.names", all = TRUE))  
+    
+    # rename column with relative abundance values for easier plotting 
+    # specify the column number for Incertae_sedis as it has multiple OTU IDs; others have only one OTU ID
+    if (taxa.names[j] == "f__Incertae_sedis") {
+      colnames(d)[44] <- "Taxa_rel_abundance" # multiple OTUs; this is the column that contains the OTU of interest
+    } else {
+      colnames(d)[ncol(d)] <- "Taxa_rel_abundance" 
+    }
+    
+    ## TNF ##
+    
+    # subset dataset
+    d.TNF <- d %>%
+      subset(select = c("Subject", "Treatment", "Timepoint_revised", "Taxa_rel_abundance")) %>%
+      filter(Treatment == "1_TNF") %>% 
+      filter(Timepoint_revised != "D")
+    
+    write.csv(d.TNF, file = paste(dir.taxa, "TNF.data.csv", sep = "/"))
+    
+    ##########
+    
+    ## IL17 loading ##
+    
+    # subset dataset
+    d.IL17.load <- d %>%
+      subset(select = c("Subject", "Treatment", "Timepoint_revised", "Taxa_rel_abundance")) %>%
+      filter(Treatment == "2_IL17") %>% 
+      filter(Timepoint_revised != "D")
+    
+    write.csv(d.IL17.load, file = paste(dir.taxa, "IL17.load.data.csv", sep = "/"))
+    
+    ##########
+    
+    ## IL17 maintenance ##
+    
+    # subset dataset
+    d.IL17.maint <- d %>%
+      subset(select = c("Subject", "Treatment", "Timepoint_revised", "Taxa_rel_abundance")) %>%
+      filter(Treatment == "2_IL17") %>% 
+      filter(Timepoint_revised != "C") %>%
+      filter(Subject != "cos8" & Subject != "cos11" & Subject != "cos14" & Subject != "cos18")
+    
+    write.csv(d.IL17.maint, file = paste(dir.taxa, "IL17.maint.data.csv", sep = "/"))
+    
+    ##########
+    
+    # calculate Wilcoxon between pre-post visits for each subset
+    wilc.TNF <- wilcox.test(Taxa_rel_abundance ~ Timepoint_revised, data = d.TNF, paired = TRUE)
+    wilc.IL17.load <- wilcox.test(Taxa_rel_abundance ~ Timepoint_revised, data = d.IL17.load, paired = TRUE)
+    wilc.IL17.maint <- wilcox.test(Taxa_rel_abundance ~ Timepoint_revised, data = d.IL17.maint, paired = TRUE)
+    
+    # store stats
+    p.val[j, 1] <- taxa.names[j]
+    p.val[j, 2] <- wilc.TNF$p.value
+    p.val[j, 3] <- wilc.IL17.load$p.value
+    p.val[j, 4] <- wilc.IL17.maint$p.value
+  }
+  
+  # save table of stats
+  write.csv(p.val, file = ".../IL17.TNF/ITS/jobs/5_specific.taxa_rel.abund.wilcox_R/p.value.table.csv")
+}
+
+############################################################################
+############################################################################
+############################################################################
+
+### Table of p-values of absolute delta relative abundance between TNFi and IL-17i ###
+
+# list of phyloseq objects to process
+pseqs <- list(physeq = phy_ITS.R1_human_TNF.B.C_IL17.B.C.D)
+
+# list of specific taxa
+taxa.names <- c("p__Ascomycota",
+                "c__Saccharomycetes", "c__Tremellomycetes",
+                "o__Saccharomycetales", "o__Tremellales",
+                "f__Saccharomycetaceae", "f__Mycosphaerellaceae", "f__Glomerellaceae",
+                "f__Plectosphaerellaceae", "f__Sclerotiniaceae", "f__Mucoraceae",
+                "f__Incertae_sedis",
+                "g__Saccharomyces", "g__Candida", "g__Penicillium", 
+                "g__Debaryomyces", "g__Glomerella", "g__Plectosphaerella",
+                "g__Aspergillus",
+                "s__Pichia_sp_1_TMS_2011", "s__Penicillium_charlesii", "s__Hypocrea_pseudokoningii",
+                "s__Fusarium_sp_4_TMS_2011", "s__Candida_fermenticarens", "s__Trichoderma_longibrachiatum",
+                "s__Rhodotorula_sp_RhSoW01", "s__Aspergillus_niger", "s__Saccharomyces_cerevisiae", 
+                "s__Candida_albicans", "s__Candida_dubliniensis", "s__Candida_parapsilosis", 
+                "s__Candida_tropicalis") 
+
+# for each phyloseq object in pseqs, calculate the relative abudnance
+# over time for specific taxa
+for (i in seq_along(pseqs)) {
+  
+  # merge phyloseq objects to each taxnomic level
+  phylum <- tax_glom(pseqs[[i]], taxrank = "Phylum", NArm = FALSE)
+  class <- tax_glom(pseqs[[i]], taxrank = "Class", NArm = FALSE)
+  order <- tax_glom(pseqs[[i]], taxrank = "Order", NArm = FALSE)
+  family <- tax_glom(pseqs[[i]], taxrank = "Family", NArm = FALSE)
+  genus <- tax_glom(pseqs[[i]], taxrank = "Genus", NArm = FALSE)
+  species <- tax_glom(pseqs[[i]], taxrank = "Species", NArm = FALSE)
+  
+  # transform to relative abundance
+  tp <- transform_sample_counts(phylum, rel_abundance)
+  tc <- transform_sample_counts(class, rel_abundance)
+  to <- transform_sample_counts(order, rel_abundance)
+  tf <- transform_sample_counts(family, rel_abundance)
+  tg <- transform_sample_counts(genus, rel_abundance)
+  ts <- transform_sample_counts(species, rel_abundance)
+  
+  # create table for storing data
+  p.val <- matrix(data = NA, nrow = 32, ncol = 3)
+  colnames(p.val) <- c("Taxa", "p.value.load", "p.value.maint")
+  
+  for (j in seq_along(taxa.names)) {
+    
+    # extract relative abundance of specific taxa
+    if (grepl("p__", taxa.names[j])) {
+      taxa.rel.abund <- subset_taxa(tp, Phylum == taxa.names[j])
+    } else if (grepl("c__", taxa.names[j])) {
+      taxa.rel.abund <- subset_taxa(tc, Class == taxa.names[j])
+    } else if (grepl("o__", taxa.names[j])) {
+      taxa.rel.abund <- subset_taxa(to, Order == taxa.names[j])
+    } else if (grepl("f__", taxa.names[j])) {
+      taxa.rel.abund <- subset_taxa(tf, Family == taxa.names[j])
+    } else if (grepl("g__", taxa.names[j])) {
+      taxa.rel.abund <- subset_taxa(tg, Genus == taxa.names[j])
+    } else if (grepl("s__", taxa.names[j])) {
+      taxa.rel.abund <- subset_taxa(ts, Species == taxa.names[j])
+    }
+    
+    # create dataset
+    d <- as.data.frame(merge(sample_data(taxa.rel.abund), t(otu_table(taxa.rel.abund)), 
+                             by = "row.names", all = TRUE))  
+    
+    # rename column with relative abundance values for easier plotting 
+    # specify the column number for Incertae_sedis as it has multiple OTU IDs; others have only one OTU ID
+    if (taxa.names[j] == "f__Incertae_sedis") {
+      colnames(d)[44] <- "Taxa_rel_abundance" # multiple OTUs; this is the column that contains the OTU of interest
+    } else {
+      colnames(d)[ncol(d)] <- "Taxa_rel_abundance" 
+    }
+    
+    ## TNF ##
+    
+    # subset dataset
+    # spread data across timepoints
+    # calculate absolute delta relative abundance
+    d.TNF <- d %>%
+      subset(select = c("Subject", "Treatment", "Timepoint_revised", "Taxa_rel_abundance")) %>%
+      filter(Treatment == "1_TNF") %>% 
+      filter(Timepoint_revised != "D") %>% 
+      spread(key = "Timepoint_revised", value = "Taxa_rel_abundance") %>%
+      mutate(Delta_rel_abundance = abs(C-B))
+    
+    # rename columns
+    colnames(d.TNF)[colnames(d.TNF)=="B"] <- "1_pre"  
+    colnames(d.TNF)[colnames(d.TNF)=="C"] <- "2_post"  
+    
+    ##########
+    
+    ## IL17 loading ##
+    
+    # subset dataset
+    # spread data across timepoints
+    # calculate absolute delta relative abundance
+    d.IL17.load <- d %>%
+      subset(select = c("Subject", "Treatment", "Timepoint_revised", "Taxa_rel_abundance")) %>%
+      filter(Treatment == "2_IL17") %>% 
+      filter(Timepoint_revised != "D") %>%
+      spread(key = "Timepoint_revised", value = "Taxa_rel_abundance") %>%
+      mutate(Delta_rel_abundance = abs(C-B))
+    
+    # rename treatment
+    d.IL17.load$Treatment <- replace(as.character(d.IL17.load$Treatment), 
+                                     as.character(d.IL17.load$Treatment) == "2_IL17", "2_IL17.load")
+    # rename columns
+    colnames(d.IL17.load)[colnames(d.IL17.load)=="B"] <- "1_pre"  
+    colnames(d.IL17.load)[colnames(d.IL17.load)=="C"] <- "2_post"
+    
+    ##########
+    
+    ## IL17 maintenance ##
+    
+    # subset dataset
+    # spread data across timepoints
+    # calculate absolute delta relative abundance
+    d.IL17.maint <- d %>%
+      subset(select = c("Subject", "Treatment", "Timepoint_revised", "Taxa_rel_abundance")) %>%
+      filter(Treatment == "2_IL17") %>% 
+      filter(Timepoint_revised != "C") %>%
+      spread(key = "Timepoint_revised", value = "Taxa_rel_abundance") %>%
+      filter(!is.na(`D`)) %>%
+      mutate(Delta_rel_abundance = abs(D-B))
+    
+    # rename treatment
+    d.IL17.maint$Treatment <- replace(as.character(d.IL17.maint$Treatment), as.character(d.IL17.maint$Treatment) == "2_IL17", "3_IL17.maint")
+    
+    # rename columns
+    colnames(d.IL17.maint)[colnames(d.IL17.maint)=="B"] <- "1_pre"  
+    colnames(d.IL17.maint)[colnames(d.IL17.maint)=="D"] <- "2_post"
+    
+    ##########
+    
+    # join subsets
+    d.final <- rbind(d.TNF, d.IL17.load, d.IL17.maint)
+    
+    # split IL17 into loading and maintenance subcategories
+    d.load <- d.final[d.final$Treatment != "3_IL17.maint", ]
+    d.maint <- d.final[d.final$Treatment != "2_IL17.load", ]
+    
+    # Mann-Whitney
+    mw.load <- wilcox.test(Delta_rel_abundance ~ Treatment, data = d.load, paired = FALSE)
+    mw.maint <- wilcox.test(Delta_rel_abundance ~ Treatment, data = d.maint, paired = FALSE)
+    
+    # store stats
+    p.val[j, 1] <- taxa.names[j]
+    p.val[j, 2] <- mw.load$p.value
+    p.val[j, 3] <- mw.maint$p.value
+  }
+  
+  # save table of stats
+  write.csv(p.val, file = ".../IL17.TNF/ITS/jobs/5_specific.taxa_delta.rel.abund_R/p.value.table.csv")
+}
+
