52a81b21ced30f97145870e2dec1ab4ea8f3bb4e
Scher Lab <57496836+scher-lab@users.noreply.github.com>
2020-01-26 22:00:08 -0500

    Update convert_to_cytoscape.py

--- convert_to_cytoscape.py
+++ convert_to_cytoscape.py
@@ -1,3 +1,27 @@
+#############################################
+## Python script                           ##
+## Project: IL17.TNF Biologics manuscript  ##
+## Figure 03                               ##
+## Supplementary figures 04a-b, 14, 15a-b  ##
+## Supplementary tables 03a-b              ##
+## 16S data                                ##
+#############################################
+
+### Brief description:
+
+### Figure 03
+### Panel A: Bacterial co-occurrence network pre-IL-17i treatment
+### Panel B: Bacterial co-occurrence network post-IL-17i treatment (maintenance visit)
+
+### Supplementary figure 04a: Bacterial co-occurrence network pre-TNFi treatment
+### Supplementary figure 04b: Bacterial co-occurrence network post-TNFi treatment (maintenance visit)
+
+### Supplementary figure 14:  Expanded bacterial co-occurrence network post-TNFi treatment (maintenance visit)
+
+### Supplementary figure 15a: Expanded bacterial co-occurrence network pre-IL-17i treatment
+### Supplementary figure 15b: Expanded bacterial co-occurrence network post-IL-17i treatment (maintenance visit)
+
+
 #!/usr/env python
 
 import pandas as pd
