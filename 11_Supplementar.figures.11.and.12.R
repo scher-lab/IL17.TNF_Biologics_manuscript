a3031abf2ad7d18214fbbd6217e2e8f99915ba49
Scher Lab <57496836+scher-lab@users.noreply.github.com>
2020-01-27 10:48:29 -0500

    Update 11_Supplementar.figures.11.and.12.R

--- 11_Supplementar.figures.11.and.12.R
+++ 11_Supplementar.figures.11.and.12.R
@@ -1,19 +1,19 @@
-##################################################################### 
-## R script                                                        ##
-## Project: IL17.TNF Biologics manuscript                          ##
-## Supplementary figures 11 and 12; Supplementary tables 07 and 08 ##
-## 16S and ITS data		                                   ##
-## Author: JM                                                      ##
-#####################################################################
+############################################
+## R script                               ##
+## Project: IL17.TNF Biologics manuscript ##
+## Supplementary figures 11,12            ##
+## Supplementary tables 07,08             ##
+## 16S and ITS data		                    ##
+############################################
 
 ### Brief description:
-### This script covers the code for Supplementary figures 11 and 12:
+
+### Supplementary figures 11, 12:
 ### Panels A: Bacterial and fungal taxa correlations w/ local proteins, cytokines and metabolites for TNFi
 ### Panels B: Bacterial and fungal taxa correlations w/ local proteins, cytokines and metabolites for IL-17i
 
-### Supplementary table 07: Statistics for local proteins and cytokines
-
-### Supplementary table 08: Statistics for local metabolites
+### Supplementary table 07: Statistics for proteins and cytokines
+### Supplementary table 08: Statistics for metabolites
 
 ############################################################################
 ############################################################################
@@ -29,21 +29,24 @@ library(reshape2)
 ############################################################################
 ############################################################################
 
-# Directory for storing files
+### Correlograms for proteins, cytokines and metabolites ###
+
+### Only genera that had relative abundance of at least 0.5% in at least two samples were maintained.
+### QIIME v1.9.1 was used to calculate Spearman correlations via the observation_metadata_correlation.py command.
+### Correlation tables (input files) were created in Microsoft Excel.
+
+### Input files:
+### Column 1: significant genera
+### Remainder of columns: Spearman's rank correlation coefficient's accross genera
+
+# directory for storing files
 dir = ".../IL17.TNF/16S.ITS/jobs/6_correlogram/"
 
+############################
 
-### Correlograms
-### Input table columns:
+## TNFi: pre-maintenance ##
 
-
-## TNF.BC ##
-
-# Pruning: genus level => > 0.5% relative abundance => at least 2 samples
-# QIIME v1.9.1 used to calculate spearman correlations via observation_metadata_correlation.py command
-# Correlation tables created in excel 
-
-# FDR < 0.1 #
+# FDR < 0.1
 d <- read.table(file = paste(dir, "TNF.B.C_cytokines_metabolites_spearman_0.1fdr.txt", sep = ""),
                 header = TRUE, row.names = 1, sep = "\t", check.names = FALSE,
                 na.strings = "NA")
@@ -53,8 +56,6 @@ corrplot(as.matrix(d), method = "circle", diag = TRUE, na.label = " ",
          is.corr = TRUE, cl.ratio = 0.18, cl.cex = 0.7,
          tl.cex = 0.7, tl.col = "black")
 dev.off()
-
-##########
 
 # FDR < 0.2 #
 d <- read.table(file = paste(dir, "TNF.B.C_cytokines_metabolites_spearman_0.2fdr.txt", sep = ""),
@@ -67,15 +68,9 @@ corrplot(as.matrix(d), method = "circle", diag = TRUE, na.label = " ",
          tl.cex = 0.7, tl.col = "black")
 dev.off()
 
-############################################################################
-############################################################################
-############################################################################
+############################
 
-## IL17.BCD ##
-
-# Pruning: genus level => > 0.5% relative abundance => at least 2 samples
-# QIIME used to calculate spearman correlations
-# Correlation tables created in excel 
+## IL-17i: pre-loading-maintenance ##
 
 # FDR < 0.1 #
 d <- read.table(file = paste(dir, "IL17.B.C.D_cytokines_metabolites_spearman_0.1fdr.txt", sep = ""),
@@ -87,8 +82,6 @@ corrplot(as.matrix(d), method = "circle", diag = TRUE, na.label = " ",
          is.corr = TRUE, cl.ratio = 0.18, cl.cex = 0.7,
          tl.cex = 0.7, tl.col = "black")
 dev.off()
-
-##########
 
 # FDR < 0.2 #
 d <- read.table(file = paste(dir, "IL17.B.C.D_cytokines_metabolites_spearman_0.2fdr.txt", sep = ""),
@@ -106,7 +99,14 @@ dev.off()
 ############################################################################
 ############################################################################
 
-### TNF cytokines ###
+### Statistics for proteins/cytokines ###
+
+### Input files:
+### Column 1: sample ID
+### Column 2: timepoint
+### Remainder of columns: protein/cytokine quantities across samples
+
+## TNFi ##
 
 # directory
 dir = ".../IL17.TNF/16S/jobs/6_cytokines_R/"
@@ -133,11 +133,9 @@ for (i in 1:13) {
 ft = paste(dir, "cytokines_TNF.B.C_wilcoxon.csv", sep = "")
 write.csv(file = ft, wilc.table)
 
-############################################################################
-############################################################################
-############################################################################
+############################
 
-### IL17 cytokines ###
+## IL17-i ###
 
 # directory
 dir = ".../IL17.TNF/16S/jobs/6_cytokines_R/"
@@ -150,7 +148,8 @@ d.cytokines <- read.table(file = paste(dir, "cytokines_IL17.B.C.D.txt", sep = ""
 # convert to data frame
 d.cytokines <- as.data.frame(d.cytokines)
 
-## pre-load ##
+##########
+
 # subset pre/loading samples
 d.load <- d.cytokines[d.cytokines$Pre_post_det != "3_maint", ]
 
@@ -160,7 +159,6 @@ d.load.fecal <- na.omit(d.load.fecal)
 d.load.fecal.25.33 <- d.load[c(1,10,12)]
 
 # create table for storing wilcoxon results
-# fecal
 wilc.table.load.fecal.a <- matrix(data = NA, nrow = 9, ncol = 2)
 colnames(wilc.table.load.fecal.a) <- c("cytokine", "pre-loading")
 
@@ -168,7 +166,6 @@ wilc.table.load.fecal.b <- matrix(data = NA, nrow = 2, ncol = 2)
 colnames(wilc.table.load.fecal.b) <- c("cytokine", "pre-loading")
 
 # calculate wilcoxon between pre/loading visits for fecal cytokines
-# fecal
 for (i in 1:9) {
   wilc.table.load.fecal.a[i,1] <- colnames(d.load.fecal)[i+1]
   wilc.table.load.fecal.a[i,2] <- wilcox.test(d.load.fecal[,i+1] ~ Pre_post_det, data = d.load.fecal, paired = TRUE)$p.value
@@ -186,8 +183,8 @@ write.csv(file = ft.load.fecal.a, wilc.table.load.fecal.a)
 ft.load.fecal.b = paste(dir, "cytokines_IL17.B.C.D_wilcoxon.loading_25.33_fecal.b.csv", sep = "")
 write.csv(file = ft.load.fecal.b, wilc.table.load.fecal.b)
 
+##########
 
-## pre-maint ##
 # subset pre/maintenance samples
 d.maint <- d.cytokines[d.cytokines$Pre_post_det != "2_load", ]
 
@@ -199,7 +196,6 @@ d.maint.fecal.25.33 <- d.maint[c(1,10,12)]
 d.maint.fecal.25.33 <- d.maint.fecal.25.33[-c(9, 12, 15, 20), ]
 
 # create table for storing wilcoxon results
-# fecal
 wilc.table.maint.fecal.a <- matrix(data = NA, nrow = 9, ncol = 2)
 colnames(wilc.table.maint.fecal.a) <- c("cytokine", "pre-maintenance")
 
@@ -207,7 +203,6 @@ wilc.table.maint.fecal.b <- matrix(data = NA, nrow = 2, ncol = 2)
 colnames(wilc.table.maint.fecal.b) <- c("cytokine", "pre-maintenance")
 
 # calculate wilcoxon between pre/maintenance visits for fecal cytokines
-# fecal
 for (i in 1:9) {
   wilc.table.maint.fecal.a[i,1] <- colnames(d.maint.fecal)[i+1]
   wilc.table.maint.fecal.a[i,2] <- wilcox.test(d.maint.fecal[,i+1] ~ Pre_post_det, data = d.maint.fecal, paired = TRUE)$p.value
@@ -229,7 +224,14 @@ write.csv(file = ft.maint.fecal.b, wilc.table.maint.fecal.b)
 ############################################################################
 ############################################################################
 
-### TNF metabolites ###
+### Statistics for metabolites ###
+
+### Input files:
+### Column 1: sample ID
+### Column 2: timepoint
+### Remainder of columns: metabolite quantities across samples
+
+## TNFi ##
 
 # directory
 dir = ".../IL17.TNF/16S/jobs/7_metabolites_R/"
@@ -256,11 +258,9 @@ for (i in 1:20) {
 ft = paste(dir, "metabolites_TNF.B.C_wilcoxon.csv", sep = "")
 write.csv(file = ft, wilc.table)
 
-############################################################################
-############################################################################
-############################################################################
+############################
 
-### IL17 metabolites ###
+## IL-17i ##
 
 # directory
 dir = ".../IL17.TNF/16S/jobs/7_metabolites_R/"
@@ -272,6 +272,8 @@ d.metabolites <- read.table(file = paste(dir, "metabolites_IL17.B.C.D.txt", sep 
 
 # convert to data frame
 d.metabolites <- as.data.frame(d.metabolites)
+
+##########
 
 # subset pre/loading samples
 d.load <- d.metabolites[d.metabolites$Pre_post_det != "3_maint", ]
@@ -289,6 +291,8 @@ for (i in 1:20) {
 # save
 ft.load = paste(dir, "metabolites_IL17.B.C.D_wilcoxon.loading.csv", sep = "")
 write.csv(file = ft.load, wilc.table.load)
+
+##########
 
 # subset pre/maintenance samples
 d.maint <- d.metabolites[d.metabolites$Pre_post_det != "2_load", ]
